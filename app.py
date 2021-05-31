f6ed015bbcce4502444d785e405d2359b1e877a9
AideenM12 <a.malone1289@gmail.com>
2021-05-31 20:43:08 +0000

    Create add_article.html template

--- app.py
+++ app.py
@@ -161,6 +161,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_article")
+def add_article():
+    return render_template("add_article.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
